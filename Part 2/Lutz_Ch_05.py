617d46189065e771f6aa2f31e23c8c72fa6c623a
Rolando-Desktop-Ice <rolandolopez3442@yahoo.com>
2015-05-21 10:59:21 -0700

    tt

--- Part 2/Lutz_Ch_05.py
+++ Part 2/Lutz_Ch_05.py
@@ -72,3 +72,16 @@ print(math.trunc(5 / -2))       # Truncate instead of floor
 print((5 / 2), (5 / 2.0), (5 / -2.0), (5 / -2))
 print((5 // 2), (5 // 2.0), (5 // -2.0), (5 // -2))
 print((9 / 3), (9.0 / 3), (9 // 3), (9 // 3.0))
+
+
+## PG 151 Hex, Octal, Binary: Literals and conversions
+
+print(0o1, 0o20, 0o377)             # Octal literals: Base 8, digits 0-7
+print(0x01, 0x10, 0xFF)             # Hex literals: Base 16, digits 0-9/A-F
+print(0b1, 0b10000, 0b11111111)     # Binary literals: Base 2, digits 0-1
+
+print(0xFF, (15 * (16 ** 1)) + (15 * (16 ** 0)))    # How hex/binary map to decimal
+print(0x2F, (2 * (16 ** 1)) + (15 * (16 ** 0)))
+print(0xF, 0b1111, (1 * (2 ** 3) + 1 * (2 ** 2) + 1 * (2 ** 1) + 1 * (2 ** 0)))
+
+print(oct(64), hex(64), bin(64))    # Numbers -> digit strings
