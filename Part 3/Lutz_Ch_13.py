fa8c1f22fdab8d760aa8309f4fe766041503c04b
Rolando-Apache <rolandolopez3442@yahoo.com>
2015-09-20 19:25:56 -0700

    ch 13

--- Part 3/Lutz_Ch_13.py
+++ Part 3/Lutz_Ch_13.py
@@ -92,3 +92,70 @@ print(a, b, c)
 for (a, *b, c) in [(1, 2, 3, 4), (5, 6, 7, 8)]:
     print(a, b, c)
 
+for all in [(1, 2, 3, 4), (5, 6, 7, 8)]:
+    a, b, c = all[0], all[1:3], all[3]
+    print(a, b, c)
+
+items = ["aaa", 111, (4, 5), 2.01]      # A set of objects
+tests = [(4, 5), 3.14]                  # Keys to search for
+
+for key in tests:
+    for item in items:
+        if item == key:
+            print(key, "was found")
+            break
+    else:
+        print(key, "not found!")
+
+for key in tests:
+    if key in items:
+        print(key, "way found")
+    else:
+        print(key, "not found!")
+
+seq1 = "spam"
+seq2 = "scam"
+
+res = []
+for x in seq1:
+    if x in seq2:
+        res.append(x)
+
+print(res)
+
+print([x for x in seq1 if x in seq2])
+
+print(list(range(5)), list(range(2, 5)), list(range(0, 10, 2)))
+print(list(range(-5, 5)))
+print(list(range(5, -5, -1)))
+
+for i in range(3):
+    print(i, 'Pythons')
+
+X = 'spam'
+for item in X:
+    print(item, end=' ')
+
+print()
+k = 0
+while k < len(X):
+    print(X[k], end=' ')
+    k += 1
+
+print("\n%s" % X)
+print(len(X))
+print(list(range(len(X))))
+for i in range(len(X)):
+    print(X[i], end=' ')
+
+print()
+for item in X:
+    print(item, end=' ')
+
+
+print()
+S = 'spam'
+
+for i in range(len(S)):
+    S = S[1:] + S[:1]
+    print(S, end=' ')
